2f60acc7903fdf8a3512222404166f26f09d56aa
Ruan Ferreira da Silva <ruan4f@gmail.com>
2014-12-02 23:13:04 -0200

    Update

--- src/negocio/Quarto.java
+++ src/negocio/Quarto.java
@@ -22,63 +22,54 @@ public class Quarto {
     public Double getMetrosQuadrados() {
         return metrosQuadrados;
     }
-
     /**
      * @param metrosQuadrados the metrosQuadrados to set
      */
     public void setMetrosQuadrados(Double metrosQuadrados) {
         this.metrosQuadrados = metrosQuadrados;
     }
-
     /**
      * @return the preco
      */
     public Double getPreco() {
         return isTemBanheira() ? (this.preco * 0.18 )+ this.preco : this.preco;
     }
-
     /**
      * @param preco the preco to set
      */
     public void setPreco(Double preco) {
         this.preco = preco;
     }
-
     /**
      * @return the reservado
      */
     public boolean isReservado() {
         return reservado;
     }
-
     /**
      * @param reservado the reservado to set
      */
     public void setReservado(boolean reservado) {
         this.reservado = reservado;
     }
-
     @Override
     public String toString() {
         //return super.toString();
         return "\nQuarto: " + this.getNumeroQuarto()  + " - Preço: R$ " + this.getPreco() 
                 + " - Possui Banheira: "+ (isTemBanheira() ? "Sim":"Não" ) + " - Disponivel: " + (isReservado() ? " Não":" Sim" );
     }
-
     /**
      * @return the numeroQuarto
      */
     public int getNumeroQuarto() {
         return numeroQuarto;
     }
-
     /**
      * @param numeroQuarto the numeroQuarto to set
      */
     public void setNumeroQuarto(int numeroQuarto) {
         this.numeroQuarto = numeroQuarto;
     }
-
     void setTemBanheira(boolean b) {
       this.temBanheira = b;
     }
@@ -88,5 +79,4 @@ public class Quarto {
     public boolean isTemBanheira() {
         return temBanheira;
     }
-
 }
